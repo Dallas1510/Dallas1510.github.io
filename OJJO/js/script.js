c4548c4cc5ba9e281701aa1e2ac7443286209e09
Dallas1510 <81421158+Dallas1510@users.noreply.github.com>
2021-06-29 13:22:57 +0300

    Add files via upload

---
+++ OJJO/js/script.js
@@ -0,0 +1,78 @@
+// --------------Sliders-----------------
+new Swiper(".preview-slider", {
+	loop: true,
+	slidesPerView: 1,
+	spaceBetween: 0,
+	slideToClickedSlide: true,
+	thumbs: {
+		swiper: {
+			el: '.mini-preview-slider',
+			loop: true,
+			slidesPerView: 2,
+			spaceBetween: 15,
+			slideToClickedSlide: true,
+			centeredSlides: true,
+			initialSlide: 1,
+			breakpoints: {
+				501: {
+					slidesPerView: 3,
+				},
+				768: {
+					slidesPerView: 3,
+					spaceBetween: 30,
+				},
+			},
+		},
+	},
+});
+new Swiper(".more-goods__swiper", {
+	slidesPerView: 1,
+	loop: true,
+	breakpoints: {
+		425: {
+			slidesPerView: 2,
+		},
+		768: {
+			slidesPerView: 3,
+		},
+	}
+});
+// ------------Search------------
+document.getElementById('nav__search').onclick = function () {
+	document.getElementById('nav__form').classList.toggle('active');
+};
+// ----------Burger-----------
+document.getElementById('burger__btn').onclick = function () {
+	document.getElementById('burger__btn').classList.toggle('active'),
+		document.getElementById('burger').classList.toggle('active'),
+		document.getElementById('lock').classList.toggle('lock');
+};
+// --------------Tabs--------------
+function openCategory(evt, categoryName) {
+	var i, tabs__content, tabs__btn;
+	tabs__content = document.getElementsByClassName("tabs__content");
+	for (i = 0; i < tabs__content.length; i++) {
+		tabs__content[i].style.display = "none";
+	}
+	tabs__btn = document.getElementsByClassName("tabs__btn");
+	for (i = 0; i < tabs__btn.length; i++) {
+		tabs__btn[i].className = tabs__btn[i].className.replace(" active", "");
+	}
+	document.getElementById(categoryName).style.display = "flex";
+	evt.currentTarget.className += " active";
+};
+// ---------Filter--------------
+$(".dropdown__btn").click(function (event) {
+	event.preventDefault();
+	$(this).parent().toggleClass("active");
+	return false;
+});
+
+// -------------ReadFull----------------
+document.getElementById('more').onclick = function () {
+	document.getElementById('full-text').classList.toggle('fulltext');
+};
+// ------------ShowMore--------------
+document.getElementById('show-more').onclick = function () {
+	document.getElementById('full-height').classList.add('more');
+};
