db7d79a898cada63c3bcf7bca96b022c43602cfa
Dallas1510 <81421158+Dallas1510@users.noreply.github.com>
2022-01-10 14:59:58 +0200

    Add files via upload

--- meow/js/script.js
+++ meow/js/script.js
@@ -5,6 +5,7 @@
 document.querySelector('.burger__btn').addEventListener('click', ()=>{
 	document.querySelector('.burger__btn').classList.toggle('active');
 	document.querySelector('.header__nav').classList.toggle('active');
+	document.querySelector('.header').classList.toggle('active');
 	document.querySelector('body').classList.toggle('lock');
 })
 
@@ -38,3 +39,45 @@ function getInputValue(){
 }
 
 document.querySelector('.adventures__btn').addEventListener('click', getInputValue);
+
+//SPOILER
+
+document.querySelector('.story__title').addEventListener('click', ()=> {
+    document.querySelector('.story__texthide').classList.toggle('hide');
+})
+
+//SCROLL TO TOP
+
+function scrollTo(to, duration = 700) {
+    const
+        element = document.scrollingElement || document.documentElement,
+        start = element.scrollTop,
+        change = to - start,
+        startDate = +new Date(),
+        easeInOutQuad = function (t, b, c, d) {
+            t /= d / 2;
+            if (t < 1) return c / 2 * t * t + b;
+            t--;
+            return -c / 2 * (t * (t - 2) - 1) + b;
+        },
+        animateScroll = function () {
+            const currentDate = +new Date();
+            const currentTime = currentDate - startDate;
+            element.scrollTop = parseInt(easeInOutQuad(currentTime, start, change, duration));
+            if (currentTime < duration) {
+                requestAnimationFrame(animateScroll);
+            }
+            else {
+                element.scrollTop = to;
+            }
+        };
+    animateScroll();
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    let btn = document.querySelector('.to-top');
+    btn.onclick = function (click) {
+        click.preventDefault();
+        scrollTo(0, 400);
+    }
+});
